26a4cc3ceb382fb4816fafbf19bde94054e2fc1e
baxter08 <baxter08@hotmail.fr>
2023-03-01 10:38:28 +0100

    instalation

--- webpack.config.js
+++ webpack.config.js
@@ -7,13 +7,28 @@ if (!Encore.isRuntimeEnvironmentConfigured()) {
 }
 
 Encore
+Encore
+    // ...
+    enablePostCssLoader()
+
     // directory where compiled assets will be stored
     .setOutputPath('public/build/')
     // public path used by the web server to access the output path
     .setPublicPath('/build')
     // only needed for CDN's or subdirectory deploy
     //.setManifestKeyPrefix('build/')
+// webpack.config.js
+const path = require('path');
 
+  Encore
+      // ...
+    enablePostCssLoader((options) => {
+        options.postcssOptions = {
+             // the directory where the postcss.config.js file is stored
+            config: path.resolve(__dirname, 'sub-dir', 'custom.config.js'),
+        };
+    })
+  
     /*
      * ENTRY CONFIG
      *
