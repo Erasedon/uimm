f6499e79a7168ef6eb7e08cd5942fba26fe8f4a8
baxter08 <baxter08@hotmail.fr>
2023-03-03 13:02:52 +0100

    partenaire test

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,12 +10,12 @@ module.exports = {
     colors: {
 
       'tahiti': {
-        100: '#112c50',
-        200: '#e8e8e8',
-        300: '#f6f6f6',
-        400: '#oooooo',
-        500: '#ef6602',
-        600: '#ffffff'
+        100: '#112c50',  /**bleu */
+        200: '#e8e8e8',  /**gris */
+        300: '#f6f6f6',  /**blanc casser */
+        400: '#oooooo',  /**noir */
+        500: '#ef6602',  /**orange */
+        600: '#ffffff'   /**blanc*/
 
       },
       // ...
