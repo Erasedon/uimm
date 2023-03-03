fad09ebdd6fb49f868efc43924d2727c582e1f83
baxter08 <baxter08@hotmail.fr>
2023-03-03 09:56:43 +0100

    fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,20 @@ module.exports = {
 
   ],
   theme: {
+
+    colors: {
+
+      'tahiti': {
+        100: '#112c50',
+        200: '#e8e8e8',
+        300: '#f6f6f6',
+        400: '#oooooo',
+        500: '#ef6602',
+        600: '#ffffff'
+
+      },
+      // ...
+    },
     extend: {},
   },
   plugins: [],
